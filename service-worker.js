69f54291ea9608695da963f983e63d301b2c9bc4
esteves023 <37459493+esteves023@users.noreply.github.com>
2025-04-03 03:53:43 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,19 @@
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('tanques-v1').then(function(cache) {
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('transferencia-cache').then(function(cache) {
       return cache.addAll([
-        './',
-        './controle_tanques_interface.html',
-        './manifest.json',
-        './service-worker.js'
+        'tanque_layout_percent_96_validacao_bidirecional.html',
+        'manifest.json',
+        'silo-icon.png'
       ]);
     })
   );
 });
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
